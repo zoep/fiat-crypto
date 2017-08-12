da4deb8c692baa51cecb6c5d6378a2448d85a692
Jason Gross <jgross@mit.edu>
2017-08-11 21:30:41 -0400

    More WIP on TLS

--- src/Spec/Trace.v
+++ src/Spec/Trace.v
@@ -1,4 +1,5 @@
 Require Import Coq.Lists.List.
+Require Import Crypto.Util.Decidable.
 
 Section Trace.
   Context {state input output : Type}
@@ -10,7 +11,7 @@ Section Trace.
                    (_:multistep s0 l s1)
                    (_:step s1 i o s2) :
       multistep s0 (l++(i,o)::nil) s2.
-
+  (* FIXME: maybe replace with invariant definition *)
   Definition allows_behavior l :=
     exists s0 s1, init s0 -> multistep s0 l s1.
 End Trace.
@@ -25,14 +26,27 @@ connections)
 
 Notation eta x := (fst x, snd x).
 
+(* TODO: multiple other messages *)
+Definition output_in_list_and_previous_input_matching_satisfies
+           {input output} (matcher : output -> Prop) (R : input -> output -> Prop)
+           trace
+  := forall message : input * output,
+    List.In message trace
+    -> matcher (snd message)
+    -> exists other_message : input * output,
+        R (fst other_message) (snd message).
+
 Section Example_1_Negotiation.
   Definition trace_satisfies_negotiation_spec
              (trace : list (list nat (*input*) * nat (*output*))) : Prop
-    := forall io, List.In io trace
-                  -> let '(i, o) := eta io in List.In o i.
+    := output_in_list_and_previous_input_matching_satisfies
+         (fun output => True)
+         (fun i o => List.In o i)
+         trace.
 End Example_1_Negotiation.
 
 Section Example_1_Negotiation_Synthesis.
+  Opaque output_in_list_and_previous_input_matching_satisfies.
   Goal { state : Type
      & { init : state -> Prop
      & { step : _
@@ -54,12 +68,22 @@ Section Example_1_Negotiation_Synthesis.
     start.
     start_unfold.
     Lemma handle_stateless {state input output init}
-          {step : state -> input -> output -> state -> Prop}
-          {trace P}
+          {trace matcher} {R : _ -> _ -> Prop}
+          (handler : input -> output)
+          (step : state -> input -> output -> state -> Prop
+           := fun _ i o _ => handler i = o)
+          (handler_ok : forall i, R i (handler i))
       : (forall st0 st1 io, step st0 (fst io) (snd io) st1
-                            -> P io)
+                            -> R (fst io) (snd io))
         -> (@allows_behavior state input output init step trace
-            -> forall v, In v trace -> P v).
+            -> output_in_list_and_previous_input_matching_satisfies
+                 matcher R trace).
+    Proof.
+      subst step; cbv beta.
+      intros Hstep Hbehavior.
+      Require Import Crypto.Util.Tactics.DestructHead.
+      destruct_head_hnf' ex.
+
     Abort.
   Abort.
 End Example_1_Negotiation_Synthesis.
