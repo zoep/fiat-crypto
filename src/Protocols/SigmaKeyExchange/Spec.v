0196c5b3fbc8da27e6ccf87589e29797b6a5ba91
Jason Gross <jgross@mit.edu>
2017-08-30 19:58:15 -0400

    More stateful constraints in Coq

--- src/Protocols/SigmaKeyExchange/Spec.v
+++ src/Protocols/SigmaKeyExchange/Spec.v
@@ -312,13 +312,87 @@ Section spec.
                    after)
              trace.
 
+      Section stateful.
+        Let secret_x_value_type := random.
+        Let longterm_secret_type := secret.
+        Context (public_state_type : Type)
+                (project_secret_x_value : state -> option secret_x_value_type)
+                (project_public : state -> public_state_type)
+                (project_longterm_secret : state -> option longterm_secret_type).
 
-(* Secrets constraint: If you recieve a randomness message, you may
-immediately emit [g^x] and update the secret-x-value with [x], or you
-may do anything which does not depend on the contents of the random
-message.  (the decision must depend only on public things) *)
-(* Secrets constraint: You may at any time emit a message matching
-long-form or short-form using the secret-x-value in the appropriate
-place and not in other places, or else you must emit somthing not
-depending on the secret-x-value (the decision must depend only on
-public things) *)
+        (* Secrets constraint: If you recieve a randomness message,
+            you may immediately emit [g^x] and update the
+            secret-x-value with [x], or you may do anything which does
+            not depend on the contents of the random message.  (the
+            decision must depend only on public things) *)
+        Definition on_recieve_random
+                   (step : input * state -> output * state)
+          : Prop
+          := forall st : input * state,
+            let '(m_out, st') := step st in
+            let '(m_in, st) := st in
+            exists (decide_emit_g_x : public_state_type -> bool)
+                   (update_public_on_emit_g_x : public_state_type -> public -> public_state_type)
+                   (emit_on_not_g_x : public_state_type -> output)
+                   (update_public_on_not_emit_g_x : public_state_type -> public_state_type),
+              forall v,
+                m_in = Random v
+                -> if decide_emit_g_x (project_public st)
+                   then (m_out
+                         = Some
+                             (SendNetwork (text_of_public (keygen v)))
+                         /\ project_secret_x_value st' = Some v
+                         /\ project_longterm_secret st' = project_longterm_secret st
+                         /\ project_public st' = update_public_on_emit_g_x (project_public st) (keygen v))
+                   else (m_out = emit_on_not_g_x (project_public st)
+                         /\ project_secret_x_value st' = project_secret_x_value st
+                         /\ project_longterm_secret st' = project_longterm_secret st
+                         /\ project_public st' = update_public_on_not_emit_g_x (project_public st)).
+
+        (* Secrets constraint: You may at any time emit a message
+            matching long-form using the secret-x-value in the
+            appropriate place and not in other places, or else you
+            must emit somthing not depending on the secret-x-value
+            except through whether or not box_open succeeds *)
+        Definition only_use_secret_x
+                   (step : input * state -> output * state)
+          : Prop
+          := forall st : input * state,
+            let '(m_out, st') := step st in
+            let '(m_in, st) := st in
+            exists (decide_emit_long : input * public_state_type -> bool)
+                   (emit_long : input -> public_state_type -> secret_x_value_type -> longterm_secret_type -> message)
+                   (emit_non_long : public_state_type
+                                    -> input
+                                    -> (public -> ciphertext -> bool (* unseal was called and also succeeds *))
+                                    -> output)
+                   (update_public : public_state_type
+                                    -> output
+                                    -> (public -> ciphertext -> bool (* unseal was called and also succeeds *))
+                                    -> public_state_type)
+                   (update_secret_x_value : option secret_x_value_type -> input -> option secret_x_value_type),
+              if decide_emit_long (m_in, project_public st)
+              then (exists our_ephemeral_secret (out_longterm_secret : longterm_secret_type),
+                       project_secret_x_value st = Some our_ephemeral_secret
+                       /\ project_longterm_secret st = Some our_longterm_secret
+                       /\ let our_ephemeral_public := keygen our_ephemeral_secret in
+                         (m_out
+                          = Some
+                              (SendNetwork (emit_long m_in (project_public st) our_ephemeral_secret our_longterm_secret)))
+                         /\ project_public st' = update_public (project_public st) m_out (fun _ _ => false)
+                         /\ project_secret_x_value st' = project_secret_x_value st
+                         /\ project_longterm_secret st' = project_longterm_secret st)
+              else (let decide_unseal
+                        := (fun p c
+                            => match project_secret_x_value st with
+                               | Some our_ephemeral_secret
+                                 => match box_open our_ephemeral_secret p c with
+                                    | Some _ => true
+                                    | None => false
+                                    end
+                               | None => false
+                               end) in
+                    m_out = emit_non_long (project_public st) m_in decide_unseal
+                    /\ project_public st' = update_public (project_public st) m_out decide_unseal
+                    /\ project_secret_x_value st' = update_secret_x_value (project_secret_x_value st) m_in
+                    /\ project_longterm_secret st' = project_longterm_secret st).
